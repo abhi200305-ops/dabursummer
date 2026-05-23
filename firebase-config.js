0dd41dd4d7aae5eafe55a1df37f2a69c1a6b2845
abhi200305-ops <abhi200305@gmail.com>
2026-05-23 09:57:17 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+/* ============================================================
+   FIREBASE CONFIG
+   ------------------------------------------------------------
+   This object is loaded by index.html before the app boots.
+   Already filled in for the "dabur-summer" Firebase project.
+   ============================================================ */
+const firebaseConfig = {
+  apiKey: "AIzaSyA2Kuh82cLw2dkc4WFszEFLbg1cKpqvnK8",
+  authDomain: "dabur-summer.firebaseapp.com",
+  projectId: "dabur-summer",
+  storageBucket: "dabur-summer.firebasestorage.app",
+  messagingSenderId: "137752829763",
+  appId: "1:137752829763:web:6cf48df765af45ad7d19f6",
+  measurementId: "G-07FBSW77H9"
+};
